2480eb0cfc0f6d1c243a733d5c835922eb43cd5a
Alexandr-Tsiulkin <alexandr.tsiulkin@gmail.com>
2021-05-20 01:08:22 +0300

    edit pom.xml for correct build

--- web-module/src/test/java/com/gmail/alexandr/tsiulkin/controller/api/ArticleAPIControllerTest.java
+++ web-module/src/test/java/com/gmail/alexandr/tsiulkin/controller/api/ArticleAPIControllerTest.java
@@ -47,4 +47,33 @@ class ArticleAPIControllerTest {
 
         verify(articleService, times(1)).getArticles();
     }
+
+  /*  @Test
+    void shouldReturnCollectionOfObjectsWhenWeRequestGEtArticles() throws Exception {
+        ShowArticleDTO showArticleDTO = new ShowArticleDTO();
+        showArticleDTO.setId(1L);
+        showArticleDTO.setDate("18-05-2021");
+        showArticleDTO.setTitle("Title");
+        showArticleDTO.setFirstName("first name");
+        showArticleDTO.setLastName("last name");
+        showArticleDTO.setShortContent("short content");
+        showArticleDTO.setFullContent("full content");
+        ShowCommentDTO showCommentDTO = new ShowCommentDTO();
+        showCommentDTO.setId(1L);
+        showCommentDTO.setFullName("full name");
+        showCommentDTO.setDate("17-05-2021");
+        showCommentDTO.setFullContent("content");
+        showArticleDTO.getComments().add(showCommentDTO);
+
+        List<ShowArticleDTO> objects = Collections.singletonList(showArticleDTO);
+
+        when(articleService.getArticles()).thenReturn(objects);
+
+        MvcResult mvcResult = mockMvc.perform(get(REST_API_USER_PATH + ARTICLES_PATH)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        log.info("content: {}",contentAsString);
+        assertThat(contentAsString).isEqualToIgnoringCase(objectMapper.writeValueAsString(objects));
+    }*/
 }
